94640115d36664ef0f887ae3cc235fca30cc3cad
mariusztt <91893778+mariusztt@users.noreply.github.com>
2021-10-05 23:06:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { Telegraf } = require('telegraf');
 
+//require('base')
+
 const bot = new Telegraf('2003983199:AAEIp-7t3TnVHXXKZp3Jl-4CBbTaRTVbdCg');
 
 bot.command('start', ctx => {
@@ -36,9 +38,31 @@ bot.action('instrukcja', ctx => {
     })
 
 })
+var t1 = "+";
+var a = Math.round(Math.random()*10);
+var b = Math.round(Math.random()*10);
+var resultInt = a+b;
+var resultStr = `${a}+${b}`
 bot.action('graj', ctx => {
     bot.telegram.sendMessage(ctx.chat.id, `A Tu bedzie gierka kiedyś`, {
     })
+    const requestanswerKeyboard = {
+        "reply_markup": {
+            "one_time_keyboard": true,
+            "keyboard": [
+                [
+                    "randomInt/resultInt","randomInt/resultInt",
+                    
+                ],
+                [
+                    "randomInt/resultInt","randomInt/resultInt"
+                ]
+                
+
+        ]
+        }
+    };
+    bot.telegram.sendMessage(ctx.chat.id, 'tutaj bedzie resultStr', requestanswerKeyboard);
 
 })
 // bot.hears('phone', (ctx, next) => {
@@ -60,11 +84,11 @@ bot.action('graj', ctx => {
 //     "reply_markup": {
 //         "one_time_keyboard": true,
 //         "keyboard": [
-//             [{
-//                 text: "My phone number",
-//                 request_contact: true,
-//                 one_time_keyboard: true
-//             }],
+            // [{
+            //     text: "My phone number",
+            //     request_contact: true,
+            //     one_time_keyboard: true
+            // }],
 //             ["Cancel"]
 //         ]
 //     }
@@ -92,16 +116,16 @@ bot.action('graj', ctx => {
 //     ctx.deleteMessage();
 //     bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
 //         reply_markup: {
-//             inline_keyboard: [
-//                 [{
-//                         text: "dog",
-//                         callback_data: 'dog'
-//                     },
-//                     {
-//                         text: "cat",
-//                         callback_data: 'cat'
-//                     }
-//                 ],
+            // inline_keyboard: [
+            //     [{
+            //             text: "dog",
+            //             callback_data: 'dog'
+            //         },
+            //         {
+            //             text: "cat",
+            //             callback_data: 'cat'
+            //         }
+            //     ],
 
 //             ]
 //         }
@@ -125,6 +149,63 @@ bot.action('graj', ctx => {
 //     })
 
 // })
+
+
+//link do tego jak wywołać to z objektu: https://stackoverflow.com/questions/49709371/get-random-variable-with-javascript
+//skrypt do drugiego pliku base.js
+
+
+// const pObj = {
+//     p1 : {
+//         a: Math.round(Math.random()*10),
+//         b: Math.round(Math.random()*10),
+//         resultStr: `${a}+${b}`,
+//         resultInt: a+b
+//     },
+//     p2 : {
+//         a: Math.round(Math.random()*100),
+//         b: Math.round(Math.random()*100),
+//         resultStr: `${a}+${b}`,
+//         resultInt: a+b
+//     },
+//     p3 : {
+//         a: Math.round(Math.random()*10),
+//         b: Math.round(Math.random()*10),
+//         resultStr: `${a}-${b}`,
+//         resultInt: a-b
+//     },
+//     p4 : {
+//         a: Math.round(Math.random()*100),
+//         b: Math.round(Math.random()*100),
+//         resultStr: `${a}-${b}`,
+//         resultInt: a-b
+//     },
+//     p5 : {
+//         a: Math.round(Math.random()*10),
+//         b: Math.round(Math.random()*10),
+//         resultStr: `${a}*${b}`,
+//         resultInt: a*b
+//     },
+//     p6 : {
+//         a: Math.round(Math.random()*20),
+//         b: Math.round(Math.random()*20),
+//         resultStr: `${a}*${b}`,
+//         resultInt: a*b
+ //   },
+    // p7 : {
+    //     a: Math.round(Math.random()*100),
+    //     b: Math.round(Math.random()*100),
+    //     resultStr: `${a}-${b}`,
+    //     resultInt: a+b
+    // },
+    // p8 : {
+    //     a: Math.round(Math.random()*100),
+    //     b: Math.round(Math.random()*100),
+    //     resultStr: `${a}-${b}`,
+    //     resultInt: a+b
+    // },
+
+}
 
 
 bot.launch();
